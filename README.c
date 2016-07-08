2875bb3b9654b979d515fb99ae2b68b260f4165a
LALITHASHREE <dlalithashree@gmail.com>
2016-07-08 12:28:27 +0530

    finish

---
+++ README.c
@@ -0,0 +1,19 @@
+# palindrome-or-not
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a,b, name[]={"madam"};
+clrscr();
+a=strrev{name};
+if(a==name)
+{
+printf("the given string is  a palindrome");
+}
+else
+{
+printf("the given string is not a palindrome");
+}
+return0;
+}
